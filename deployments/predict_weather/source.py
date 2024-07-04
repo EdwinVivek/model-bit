10c5c1dd847f841b3a087dfe65b2759241961fc9
Edwin Vivek <nedwinvivek@gmail.com>
2024-07-04 08:20:21 +0000

    Deployed `predict_weather`

---
+++ deployments/predict_weather/source.py
@@ -0,0 +1,16 @@
+import modelbit, sys
+from typing import *
+import random
+
+# main function
+def predict_weather(days_from_now: int):
+  prediction = random.choice(["sunny", "cloudy", "just right"])
+  return {
+    "weather": prediction,
+    "message": f"In {days_from_now} days it will be {prediction}!"
+  }
+
+# to run locally via git & terminal, uncomment the following lines
+# if __name__ == "__main__":
+#   result = predict_weather(...)
+#   print(result)
