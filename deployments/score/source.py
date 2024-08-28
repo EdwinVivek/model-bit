63235a131f0831e1d8b07c5779409ca6b3c164cd
Edwin Vivek <nedwinvivek@gmail.com>
2024-08-28 19:31:15 +0000

    Deployed `score`

--- deployments/score/source.py
+++ deployments/score/source.py
@@ -1,17 +1,18 @@
 import modelbit, sys
 from typing import *
-from sklearn.linear_model._base import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.compose._column_transformer import ColumnTransformer
+from sklearn.ensemble._forest import RandomForestRegressor
 import pandas as pd
 import numpy as np
 
-lreg = modelbit.load_value("data/lreg.pkl") # LinearRegression()
+pipeline = modelbit.load_value("data/pipeline.pkl") # Pipeline(steps=[('col_transformer', ColumnTransformer(n_jobs=-1, transformers=[('cat_trans', Pipeline(steps=[('oneH', OneHotEncoder(sparse_output=False))]), ['season', 'yr', 'mnth', 'hr', 'holiday', '...
 
 # main function
 def score(lead_features: pd.DataFrame) -> np.ndarray:
-    return lreg.predict(lead_features)
+    return pipeline.predict(lead_features)
 
 # to run locally via git & terminal, uncomment the following lines
 # if __name__ == "__main__":
-#   import pandas as pd
-#   result = score(pd.DataFrame(...))
+#   result = score(...)
 #   print(result)
